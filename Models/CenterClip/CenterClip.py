22e328725e83142fa7a148778b6e016e8cf33b3e
wanger <80301089+wanger521@users.noreply.github.com>
2023-08-02 00:25:34 +0800

    Add files via upload
    addtional synthetic experiments

--- Models/CenterClip/CenterClip.py
+++ Models/CenterClip/CenterClip.py
@@ -1,6 +1,6 @@
 import numpy as np
 from FatherModel import Softmax
-
+from Config_artificial import regular, byzantine
 
 class CenterClipWorkerSoftmax(Softmax):
     def __init__(self, w, id, select, config):
@@ -22,9 +22,24 @@ class CenterClipServerSoftmax(Softmax):
         self.messages = messages
         self.last_agg = last_agg
 
+    def get_true_threshold(self):
+        # find the bottom-(honest-size) weights as the estimated threshold
+        local_model = self.last_agg
+
+        weighted_avg_norm = sum([
+            np.linalg.norm((self.messages[n] - local_model))
+            for n in regular
+        ])/len(regular)
+        return np.sqrt(weighted_avg_norm * len(byzantine))
+
     def clip(self):
         nodesize = self.config['nodeSize']
-        tau = self.config['tau']
+        if self.config['get_true'] == True:
+            tau = self.get_true_threshold()
+        else:
+            tau = self.config['tau']
+        if tau == 0:
+            return np.mean(np.array(self.messages))
         iner_iter = self.config['iner_iter']
         inil = self.last_agg
         for iiter in range(iner_iter):
